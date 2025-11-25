97e90dc93336dd0ef5fbd99adc8d138b44f3b001
李 忠斌 <lizb@tsintergy.com>
2025-11-25 13:10:07 +0800

    fix：第一次提交

---
+++ forge.config.js
@@ -0,0 +1,69 @@
+const { FusesPlugin } = require('@electron-forge/plugin-fuses');
+const { FuseV1Options, FuseVersion } = require('@electron/fuses');
+
+module.exports = {
+  packagerConfig: {
+    asar: true,
+  },
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {
+        name: 'ElectronApp',
+        authors: 'lizb',
+        setupExe: 'ElectronAppSetup.exe',
+        // setupIcon: 'path/to/icon.ico', // 如果有图标
+        // iconUrl: 'https://your-icon-url.com/icon.ico', // 远程图标URL
+        // loadingGif: 'path/to/loading.gif', // 安装过程动画（可选）
+        noMsi: true,
+        // certificateFile: '', // 如需代码签名
+        // certificatePassword: '',
+        // 使用相对路径输出
+        outputDirectory: 'out/windows',
+      },
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {},
+    },
+  ],
+  plugins: [
+    {
+      name: '@electron-forge/plugin-auto-unpack-natives',
+      config: {},
+    },
+    // Fuses are used to enable/disable various Electron functionality
+    // at package time, before code signing the application
+    new FusesPlugin({
+      version: FuseVersion.V1,
+      [FuseV1Options.RunAsNode]: false,
+      [FuseV1Options.EnableCookieEncryption]: true,
+      [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
+      [FuseV1Options.EnableNodeCliInspectArguments]: false,
+      [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
+      [FuseV1Options.OnlyLoadAppFromAsar]: true,
+    }),
+  ],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'LiZhongBin817',
+          name: 'ElectronApp'
+        },
+        prerelease: false,
+        draft: true
+      }
+    }
+  ]
+};
